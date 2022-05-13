1c5da36118c4f8282dbe6ea7369e9ae61a2b7dc1
Kyle Chen <kylecn94@google.com>
2022-05-13 12:18:47 +1000

    change

--- set2/exercise2.py
+++ set2/exercise2.py
@@ -7,16 +7,14 @@ linter with no errors or warnings!
 """
 
 import string
-
+x = 5
 def getLetter(index):
     alphabet = string.ascii_lowercase + " "
     return alphabet[index]
 
 
 def set2exersise2():
-    indices = [12, 2, 26, 7, 0, 12, 12, 4, 17]
-    for i in indices:
-        i = int(i)
+    indices = [12.0, 2, 26, 7, 0, 12, 12, 4, 17]
     wordArray = list(map(getLetter,indices))
     wordArray[0] = wordArray[0].upper()
     wordArray[1] = wordArray[1].upper()
