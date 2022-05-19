02457f93196af4f77a3eb6cb368a863f234223e4
Kyle Chen <kylecn94@google.com>
2022-05-19 14:19:07 +1000

    Set 4 Excercise 3

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -76,27 +76,116 @@ def wordy_pyramid():
     pass
 
 
-def pokedex(low=1, high=5):
-    """ Return the name, height and weight of the tallest pokemon in the range low to high.
+def sortPokedex(low=1, high=5):
+    """Return the name, height and weight of the tallest pokemon in the range low to high.
 
     Low and high are the range of pokemon ids to search between.
     Using the Pokemon API: https://pokeapi.co get some JSON using the request library
     (a working example is filled in below).
     Parse the json and extract the values needed.
-    
+
     TIP: reading json can someimes be a bit confusing. Use a tool like
          http://www.jsoneditoronline.org/ to help you see what's going on.
     TIP: these long json accessors base["thing"]["otherThing"] and so on, can
          get very long. If you are accessing a thing often, assign it to a
          variable and then future access will be easier.
     """
-    template = "https://pokeapi.co/api/v2/pokemon/{id}"
 
-    url = template.format(id=5)
-    r = requests.get(url)
-    if r.status_code is 200:
-        the_json = json.loads(r.text)
-    return {"name": None, "weight": None, "height": None}
+    pokedex = []
+    for i in range(low, high, 1):
+        url = f"https://pokeapi.co/api/v2/pokemon/{i}"
+        r = requests.get(url)
+        if r.status_code is 200:  # if the request comes back...
+            print(r)
+            data = r.json()
+            pokedex.append(
+                {
+                    "name": data["name"],
+                    "weight": data["weight"],
+                    "height": data["height"],
+                }
+            )
+    pokedex.sort(key=lambda x: x["height"], reverse=True)
+    tallestPoke = pokedex[0]
+
+    return tallestPoke
+
+
+sortPokedex(1, 5)
+
+
+def pokedex(low=1, high=5):
+    """Return the name, height and weight of the tallest pokemon in the range low to high.
+
+    Low and high are the range of pokemon ids to search between.
+    Using the Pokemon API: https://pokeapi.co get some JSON using the request library
+    (a working example is filled in below).
+    Parse the json and extract the values needed.
+
+    TIP: reading json can someimes be a bit confusing. Use a tool like
+         http://www.jsoneditoronline.org/ to help you see what's going on.
+    TIP: these long json accessors base["thing"]["otherThing"] and so on, can
+         get very long. If you are accessing a thing often, assign it to a
+         variable and then future access will be easier.
+    """
+
+    highiest = -1.0
+    tallestPoke = "kyle"
+    for i in range(low, high, 1):
+        url = f"https://pokeapi.co/api/v2/pokemon/{i}"
+        r = requests.get(url)
+        if r.status_code is 200:  # if the request comes back...
+            print(r)
+            data = r.json()
+            p = {
+                "name": data["name"],
+                "weight": data["weight"],
+                "height": data["height"],
+            }
+            if p["height"] > highiest:
+                highiest = p["height"]
+                tallestPoke = p
+
+    return tallestPoke
+
+
+def smallPokedex(low=1, high=5):
+    """Return the name, height and weight of the tallest pokemon in the range low to high.
+
+    Low and high are the range of pokemon ids to search between.
+    Using the Pokemon API: https://pokeapi.co get some JSON using the request library
+    (a working example is filled in below).
+    Parse the json and extract the values needed.
+
+    TIP: reading json can someimes be a bit confusing. Use a tool like
+         http://www.jsoneditoronline.org/ to help you see what's going on.
+    TIP: these long json accessors base["thing"]["otherThing"] and so on, can
+         get very long. If you are accessing a thing often, assign it to a
+         variable and then future access will be easier.
+    """
+
+    pokedex = []
+    for i in range(low, high, 1):
+        url = f"https://pokeapi.co/api/v2/pokemon/{i}"
+        r = requests.get(url)
+        if r.status_code is 200:  # if the request comes back...
+            print(r)
+            data = r.json()
+            pokedex.append(
+                {
+                    "name": data["name"],
+                    "weight": data["weight"],
+                    "height": data["height"],
+                }
+            )
+    highiest = -1.0
+    tallestPoke = "kyle"
+    for p in pokedex:
+        if p["height"] > highiest:
+            highiest = p["height"]
+            tallestPoke = p
+
+    return tallestPoke
 
 
 def diarist():
